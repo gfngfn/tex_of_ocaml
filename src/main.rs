9072e71ddd5e2946a1375bd3bca1a5094d63211c
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-11-29 19:02:25 +0900

    use the derive macro of 'clap'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-use clap::{App, Arg};
+/* use std::path::PathBuf; */
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "tex_of_ocaml",
+    version = "0.1.0",
+    author = "Takashi SUWA",
+    about = "A compiler from lambda terms into TeX sources",
+)]
+struct Opts {
+/*    #[clap(name = "INPUT", parse(from_os_str))] */
+    #[clap(name = "INPUT")]
+    input: Option<String>,
+
+    #[clap(short, long)]
+    output: Option<String>,
+}
 
 fn main() {
+    let opts = Opts::parse();
+/*
     let matches =
         App::new("tex_of_ocaml")
             .version("0.1.0")
@@ -17,7 +36,8 @@ fn main() {
                     .required(false)
             )
             .get_matches();
-    let input = matches.value_of("input").unwrap_or("(no input)");
-    let output = matches.value_of("output").unwrap_or("(no output)");
+*/
+    let input = opts.input.unwrap_or("(no input)".to_string());
+    let output = opts.output.unwrap_or("(no output)".to_string());
     println!("Hello, world! (input: {}, output: {})", input, output);
 }
