9de90d1adad22e12f8e4cb3628faec4a8a25c04f
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-11-29 17:54:32 +0900

    introduce 'clap@2.33.1' for handling command lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches =
+        App::new("tex_of_ocaml")
+            .version("0.1.0")
+            .arg(
+                Arg::with_name("input")
+                    .index(1)
+                    .required(false)
+            )
+            .arg(
+                Arg::with_name("output")
+                    .long("output")
+                    .short("o")
+                    .takes_value(true)
+                    .required(false)
+            )
+            .get_matches();
+    let input = matches.value_of("input").unwrap_or("(no input)");
+    let output = matches.value_of("output").unwrap_or("(no output)");
+    println!("Hello, world! (input: {}, output: {})", input, output);
 }
