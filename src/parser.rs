3c8b90389718fa2d2e38dd7caceaf7b8c568d28a
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-12-06 04:13:50 +0900

    develop the parser about applications

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,50 @@
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while, take_while1},
+    combinator::{map, opt},
+    sequence::tuple,
     IResult,
 };
 use std::char;
 
 use crate::syntax::{Expr, Ident};
+
+enum List<T> {
+    Nil,
+    Cons(T, Box<List<T>>),
+}
+
+impl<T> List<T> {
+    fn len(&self) -> usize {
+        match self {
+            List::Nil => 0,
+            List::Cons(_, ref tail) => 1 + tail.len(),
+        }
+    }
+
+    fn foldl<A>(self, f: fn(A, T) -> A, acc: A) -> A {
+        match self {
+            List::Nil => acc,
+            List::Cons(x, tail) => tail.foldl(f, f(acc, x)),
+        }
+    }
+}
+
+/*
+impl<T> Iterator for List<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        match self {
+            List::Nil => None,
+            List::Cons(x, tail) => {
+                *self = tail;
+                Some(x)
+            }
+        }
+    }
+}
+*/
 
 type Input = str;
 
@@ -39,7 +78,23 @@ fn parse_abstraction(s: &Input) -> IResult<&Input, Expr> {
 }
 
 fn parse_application(s: &Input) -> IResult<&Input, Expr> {
-    (parse_single)(s) /* TODO */
+    let (s, (e, eargs)) = parse_single_list(s)?;
+    if eargs.len() == 0usize {
+        Ok((s, e))
+    } else {
+        let eret: Expr = eargs.foldl(|eapp, earg| Expr::Apply(Box::new(eapp), Box::new(earg)), e);
+        Ok((s, eret))
+    }
+}
+
+fn parse_single_list<'a>(s: &'a Input) -> IResult<&'a Input, (Expr, List<Expr>)> {
+    map(
+        tuple((parse_single, opt(tuple((skip_space, parse_single_list))))),
+        |(e, tail_opt)| match tail_opt {
+            Some(((), (e2, eargs))) => (e, List::Cons(e2, Box::new(eargs))),
+            None => (e, List::Nil),
+        },
+    )(s)
 }
 
 fn parse_single(s: &Input) -> IResult<&Input, Expr> {
