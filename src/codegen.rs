0ca610d9a54b52d73aaa5d73f13d6e14a1da4cb8
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2020-12-06 20:07:54 +0900

    FIRST SUCCESS in the evaluation of target code

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,8 +1,17 @@
 use crate::list::List;
 use crate::syntax::{Const, Instruction};
 
+const PREFIX: &str = "\\secd@";
+const DOCUMENT_CLASS: &str = "jsarticle";
+const VM_PACKAGE_NAME: &str = "secd";
+const RESULT_NAME: &str = "result";
+
 pub fn output(instrs: List<Instruction>) -> String {
-    iter(instrs)
+    let code: String = iter(instrs);
+    format!("\\documentclass{{{}}}\\usepackage{{{}}}\\makeatletter\\edef\\{}{{{}EvalAndShow{{{}}}}}\\makeatother\\begin{{document}}\\{}\\end{{document}}",
+        DOCUMENT_CLASS, VM_PACKAGE_NAME, RESULT_NAME,
+        PREFIX, code, RESULT_NAME
+    )
 }
 
 fn iter(instrs: List<Instruction>) -> String {
@@ -10,26 +19,25 @@ fn iter(instrs: List<Instruction>) -> String {
 }
 
 fn accumulate(acc: String, instr: Instruction) -> String {
-    let prefix = "\\secd@";
     let s: String = match instr {
         Instruction::Access(i) => {
             let stars = (0..i).map(|_| "*").collect::<String>();
-            format!("{}ACCESS{{{}}}", prefix, stars)
+            format!("{}ACCESS{{{}}}", PREFIX, stars)
         }
         Instruction::Closure(instrs_sub) => {
             let s_sub = iter(*instrs_sub);
-            format!("{}CLOSURE{{{}}}", prefix, s_sub)
+            format!("{}CLOSURE{{{}}}", PREFIX, s_sub)
         }
-        Instruction::Return => format!("{}RETURN{{}}", prefix),
-        Instruction::Apply => format!("{}APPLY{{}}", prefix),
+        Instruction::Return => format!("{}RETURN{{}}", PREFIX),
+        Instruction::Apply => format!("{}APPLY{{}}", PREFIX),
         Instruction::Const(c) => match c {
             Const::Int(n) => {
                 let os = (0..n).map(|_| "o").collect::<String>();
-                format!("{}CONST{{{}INT{{{}}}{}ENDVAL}}", prefix, prefix, os, prefix)
+                format!("{}CONST{{{}INT{{{}}}{}ENDVAL}}", PREFIX, PREFIX, os, PREFIX)
             }
             Const::String(s) => format!(
                 "{}CONST{{{}STRING{{{}}}{}ENDVAL}}",
-                prefix, prefix, s, prefix
+                PREFIX, PREFIX, s, PREFIX
             ),
         },
     };
